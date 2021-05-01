698b278eba3a6fa03021b2422ad3f7f1d4bc8d9e
ajaysharma12799 <ajaysharma12799@gmail.com>
2021-05-01 14:41:32 +0530

    Improved SplashScreen UI, Improved Routing Method

--- lib/screens/splashScreen.dart
+++ lib/screens/splashScreen.dart
@@ -1,91 +1,78 @@
-import 'package:authentication_app/screens/signin.dart';
-import 'package:authentication_app/screens/signup.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-class SplashScreen extends StatefulWidget {
-  @override
-  _SplashScreenState createState() => _SplashScreenState();
-}
+class SplashScreen extends StatelessWidget {
+  final String svgImage = "assets/images/coding.svg";
 
-class _SplashScreenState extends State<SplashScreen> {
-  // Function to Navigate to Login Page
-  void navigateToLogin() {
-    Navigator.of(context)
-        .pushReplacement(MaterialPageRoute(builder: (context) => Signin()));
+  // Method to Navigate to Login Page
+  void navigateToLogin(BuildContext context) {
+    Navigator.popAndPushNamed(context, "/Signin");
   }
 
-  // Function to Navigate to Signup Page
-  void navigateToSignup() {
-    Navigator.of(context)
-        .pushReplacement(MaterialPageRoute(builder: (context) => Signup()));
+  // Method to Navigate to Signup Page
+  void navigateToSignup(BuildContext context) {
+    Navigator.popAndPushNamed(context, "/Signup");
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
       appBar: AppBar(
         elevation: 0.0,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              "Autheee Application",
-              style: TextStyle(color: Colors.purple),
-            ),
-          ],
-        ),
-        backgroundColor: Colors.white,
+        backgroundColor: Theme.of(context).primaryColor,
+        centerTitle: true,
+        title: Text("Autheee", style: TextStyle(color: Colors.white),),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        child:  Column(
           children: <Widget>[
-            Icon(
-              Icons.person,
-              size: 200.0,
-            ),
-            SizedBox(
-              height: 10.0,
+            Container(
+              width: (MediaQuery.of(context).size.width as double ) / 1.5,
+              height: (MediaQuery.of(context).size.height as double) / 1.5,
+              child: SvgPicture.asset(svgImage),
             ),
             Container(
-              width: 350.0,
+              width: (MediaQuery.of(context).size.width as double ) / 1.2,
+              height: (MediaQuery.of(context).size.height as double) / 20,
               child: ElevatedButton(
-                child: Text(
-                  "Login",
+                child: Text("Login",
                   style: TextStyle(
-                    fontSize: 20.0,
+                      fontFamily: "Quicksand",
+                      fontSize: 20,
+                      color: Theme.of(context).primaryColor,
                   ),
                 ),
+                onPressed: () => navigateToLogin(context),
                 style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(Colors.purple),
-                    foregroundColor: MaterialStateProperty.all(Colors.white),
-                    padding: MaterialStateProperty.all(EdgeInsets.all(10.0)),
-                    shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(50.0),
-                    ))),
-                onPressed: navigateToLogin,
+                  backgroundColor: MaterialStateProperty.all(Colors.white),
+                  shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50)
+                  )),
+                ),
               ),
             ),
             SizedBox(
-              height: 10.0,
+              height: (MediaQuery.of(context).size.height as double) / 35,
             ),
             Container(
-              width: 350.0,
+              width: (MediaQuery.of(context).size.width as double ) / 1.2,
+              height: (MediaQuery.of(context).size.height as double) / 20,
               child: ElevatedButton(
-                child: Text(
-                  "Signup",
+                child: Text("Signup",
                   style: TextStyle(
-                    fontSize: 20.0,
+                      fontFamily: "Quicksand",
+                      fontSize: 20,
+                      color: Theme.of(context).primaryColor
                   ),
                 ),
+                onPressed: () => navigateToSignup(context),
                 style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(Colors.purple),
-                    foregroundColor: MaterialStateProperty.all(Colors.white),
-                    padding: MaterialStateProperty.all(EdgeInsets.all(10.0)),
-                    shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(50.0),
-                    ))),
-                onPressed: navigateToSignup,
+                  backgroundColor: MaterialStateProperty.all(Colors.white),
+                  shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50)
+                  )),
+                ),
               ),
             ),
           ],
