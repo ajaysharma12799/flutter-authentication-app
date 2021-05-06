488e94524280eb4570ff6fe8b2ff92ca5b108cb9
ajaysharma12799 <ajaysharma12799@gmail.com>
2021-05-06 21:48:04 +0530

    Show Email_&_Avatar while Login

--- lib/widget/customDrawer.dart
+++ lib/widget/customDrawer.dart
@@ -1,17 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-class CustomDrawer extends StatelessWidget {
+class CustomDrawer extends StatefulWidget {
+  @override
+  _CustomDrawerState createState() => _CustomDrawerState();
+}
+
+class _CustomDrawerState extends State<CustomDrawer> {
   // Method to Navigate to SigninScreen
   void navigateToLogin(BuildContext context) {
     Navigator.of(context).pop();
     Navigator.popAndPushNamed(context, "/Signin");
   }
 
+  User user;
+
   void signoutHelper(BuildContext context) async {
     print(FirebaseAuth.instance.currentUser);
     await FirebaseAuth.instance.signOut();
     navigateToLogin(context);
+  }
+
+  loadCurrentUser() async {
+    User currentUser = await _getCurrentUser();
+    // print(currentUser);
+    setState(() {
+      this.user = currentUser;
+    });
+  }
+
+  Future _getCurrentUser() async {
+    return await FirebaseAuth.instance.currentUser;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    this.loadCurrentUser();
+  }
+
+  String initialName() {
+    String nameInitial = user.email.substring(0, 1).toUpperCase();
+    return nameInitial;
   }
 
   @override
@@ -20,10 +50,9 @@ class CustomDrawer extends StatelessWidget {
       child: ListView(
         children: <Widget>[
           UserAccountsDrawerHeader(
-            accountName: Text("accountName"),
-            accountEmail: Text("accountEmail@gmail.com"),
+            accountEmail: Text(user.email),
             currentAccountPicture: CircleAvatar(
-              child: Icon(Icons.person),
+              child: Text(initialName()),
             ),
           ),
           ListTile(
