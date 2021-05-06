7a28ac3e44f80422bf2c03b673e141dd65f618f7
ajaysharma12799 <ajaysharma12799@gmail.com>
2021-05-06 10:53:42 +0530

    Implemented Signout Feature

--- lib/widget/customDrawer.dart
+++ lib/widget/customDrawer.dart
@@ -1,6 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class CustomDrawer extends StatelessWidget {
+  // Method to Navigate to SigninScreen
+  void navigateToLogin(BuildContext context) {
+    Navigator.of(context).pop();
+    Navigator.popAndPushNamed(context, "/Signin");
+  }
+
+  void signoutHelper(BuildContext context) async {
+    print(FirebaseAuth.instance.currentUser);
+    await FirebaseAuth.instance.signOut();
+    navigateToLogin(context);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Drawer(
@@ -16,7 +29,9 @@ class CustomDrawer extends StatelessWidget {
           ListTile(
             leading: Icon(Icons.logout),
             title: Text("Logout"),
-            onTap: () {},
+            onTap: () {
+              signoutHelper(context);
+            },
           ),
         ],
       ),
